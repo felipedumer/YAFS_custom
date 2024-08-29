ce7e606aac46892dbcdac849f2b1e030dc2968ef
Felipe Ferreira Dumer <felipe.dumer@wexinc.com>
2024-08-28 23:15:37 -0300

    identation on tutorial3

--- examples/Tutorial/main3.py
+++ examples/Tutorial/main3.py
@@ -3,6 +3,7 @@
     @author: isaac
 
 """
+
 import random
 import networkx as nx
 import argparse
@@ -11,7 +12,7 @@ import time
 import numpy as np
 
 from yafs.core import Sim
-from yafs.application import Application,Message
+from yafs.application import Application, Message
 
 from yafs.population import *
 from yafs.topology import Topology
@@ -25,20 +26,31 @@ from evolutivePopulationTopology import SimpleDynamicChanges
 
 RANDOM_SEED = 1
 
+
 def create_application():
     # APLICATION
     a = Application(name="SimpleCase")
 
     # (S) --> (ServiceA) --> (A)
-    a.set_modules([{"Sensor":{"Type":Application.TYPE_SOURCE}},
-                   {"ServiceA": {"RAM": 10, "Type": Application.TYPE_MODULE}},
-                   {"Actuator": {"Type": Application.TYPE_SINK}}
-                   ])
+    a.set_modules(
+        [
+            {"Sensor": {"Type": Application.TYPE_SOURCE}},
+            {"ServiceA": {"RAM": 10, "Type": Application.TYPE_MODULE}},
+            {"Actuator": {"Type": Application.TYPE_SINK}},
+        ]
+    )
     """
     Messages among MODULES (AppEdge in iFogSim)
     """
-    m_a = Message("M.A", "Sensor", "ServiceA", instructions=20*10**6, bytes=1000)
-    m_b = Message("M.B", "ServiceA", "Actuator", instructions=30*10**6, bytes=500,broadcasting=True)
+    m_a = Message("M.A", "Sensor", "ServiceA", instructions=20 * 10**6, bytes=1000)
+    m_b = Message(
+        "M.B",
+        "ServiceA",
+        "Actuator",
+        instructions=30 * 10**6,
+        bytes=500,
+        broadcasting=True,
+    )
 
     """
     Defining which messages will be dynamically generated # the generation is controlled by Population algorithm
@@ -54,24 +66,59 @@ def create_application():
     return a
 
 
-
 def create_json_topology():
     """
-       TOPOLOGY DEFINITION
+    TOPOLOGY DEFINITION
 
-       Some attributes of fog entities (nodes) are approximate
-       """
+    Some attributes of fog entities (nodes) are approximate
+    """
 
     ## MANDATORY FIELDS
     topology_json = {}
     topology_json["entity"] = []
     topology_json["link"] = []
 
-    cloud_dev    = {"id": 0, "model": "cloud",          "mytag":"cloud", "IPT": 5000 * 10 ** 6, "RAM": 40000,"COST": 3,"WATT":20.0}
-    sensor_dev1  = {"id": 1, "model": "sensor-device-1", "IPT": 100* 10 ** 6, "RAM": 4000,"COST": 3,"WATT":40.0}
-    sensor_dev2  = {"id": 2, "model": "sensor-device-2", "IPT": 100 * 10 ** 6, "RAM": 4000, "COST": 3, "WATT": 40.0}
-    sensor_dev3  = {"id": 3, "model": "sensor-device-3", "IPT": 100 * 10 ** 6, "RAM": 4000, "COST": 3, "WATT": 40.0}
-    actuator_dev = {"id": 4, "model": "actuator-device", "IPT": 100 * 10 ** 6, "RAM": 4000,"COST": 3, "WATT": 40.0}
+    cloud_dev = {
+        "id": 0,
+        "model": "cloud",
+        "mytag": "cloud",
+        "IPT": 5000 * 10**6,
+        "RAM": 40000,
+        "COST": 3,
+        "WATT": 20.0,
+    }
+    sensor_dev1 = {
+        "id": 1,
+        "model": "sensor-device-1",
+        "IPT": 100 * 10**6,
+        "RAM": 4000,
+        "COST": 3,
+        "WATT": 40.0,
+    }
+    sensor_dev2 = {
+        "id": 2,
+        "model": "sensor-device-2",
+        "IPT": 100 * 10**6,
+        "RAM": 4000,
+        "COST": 3,
+        "WATT": 40.0,
+    }
+    sensor_dev3 = {
+        "id": 3,
+        "model": "sensor-device-3",
+        "IPT": 100 * 10**6,
+        "RAM": 4000,
+        "COST": 3,
+        "WATT": 40.0,
+    }
+    actuator_dev = {
+        "id": 4,
+        "model": "actuator-device",
+        "IPT": 100 * 10**6,
+        "RAM": 4000,
+        "COST": 3,
+        "WATT": 40.0,
+    }
 
     link1 = {"s": 0, "d": 1, "BW": 1, "PR": 10}
     link2 = {"s": 0, "d": 4, "BW": 1, "PR": 1}
@@ -87,7 +134,6 @@ def create_json_topology():
     return topology_json
 
 
-
 # @profile
 def main(simulated_time):
 
@@ -96,7 +142,7 @@ def main(simulated_time):
 
     folder_results = Path("results/")
     folder_results.mkdir(parents=True, exist_ok=True)
-    folder_results = str(folder_results)+"/"
+    folder_results = str(folder_results) + "/"
 
     """
     TOPOLOGY from a json
@@ -104,7 +150,9 @@ def main(simulated_time):
     t = Topology()
     t_json = create_json_topology()
     t.load(t_json)
-    nx.write_gexf(t.G,folder_results+"graph_main2") # you can export the Graph in multiples format to view in tools like Gephi, and so on.
+    nx.write_gexf(
+        t.G, folder_results + "graph_main2"
+    )  # you can export the Graph in multiples format to view in tools like Gephi, and so on.
 
     """
     APPLICATION
@@ -114,30 +162,40 @@ def main(simulated_time):
     """
     PLACEMENT algorithm
     """
-    placement = CloudPlacement("onCloud") # it defines the deployed rules: module-device
+    placement = CloudPlacement(
+        "onCloud"
+    )  # it defines the deployed rules: module-device
     placement.scaleService({"ServiceA": 2})
 
     """
     POPULATION algorithm
     """
-    #In ifogsim, during the creation of the application, the Sensors are assigned to the topology, in this case no. As mentioned, YAFS differentiates the adaptive sensors and their topological assignment.
-    #In its case, they use a statical assignment.
+    # In ifogsim, during the creation of the application, the Sensors are assigned to the topology, in this case no. As mentioned, YAFS differentiates the adaptive sensors and their topological assignment.
+    # In its case, they use a statical assignment.
     dDistribution = deterministic_distribution(name="Deterministic", time=100)
-    pop = SimpleDynamicChanges(2,name="Dynamic",activation_dist=dDistribution)
+    pop = SimpleDynamicChanges(2, name="Dynamic", activation_dist=dDistribution)
 
-    #, activation_dist = deterministicDistribution, time_shift = 100.0)
-    #For each type of sink modules we set a deployment on some type of devices
-    #A control sink consists on:
+    # , activation_dist = deterministicDistribution, time_shift = 100.0)
+    # For each type of sink modules we set a deployment on some type of devices
+    # A control sink consists on:
     #  args:
     #     model (str): identifies the device or devices where the sink is linked
     #     number (int): quantity of sinks linked in each device
     #     module (str): identifies the module from the app who receives the messages
-    pop.set_sink_control({"model": "actuator-device","number":2,"module":app.get_sink_modules()})
+    pop.set_sink_control(
+        {"model": "actuator-device", "number": 2, "module": app.get_sink_modules()}
+    )
 
-    #In addition, a source includes a distribution function:
+    # In addition, a source includes a distribution function:
     dDistribution2 = deterministic_distribution(name="Deterministic2", time=100)
-    pop.set_src_control({"model": "sensor-device-1", "number":1,"message": app.get_message("M.A"), "distribution": dDistribution2})
-
+    pop.set_src_control(
+        {
+            "model": "sensor-device-1",
+            "number": 1,
+            "message": app.get_message("M.A"),
+            "distribution": dDistribution2,
+        }
+    )
 
     """
     Topology and Changes on the population dynamically
@@ -145,14 +203,11 @@ def main(simulated_time):
     Population, Placement can be run in simulation time according with a distribution function
     """
 
-
-
-
     """--
     SELECTOR algorithm
     """
-    #Their "selector" is actually the shortest way, there is not type of orchestration algorithm.
-    #This implementation is already created in selector.class,called: First_ShortestPath
+    # Their "selector" is actually the shortest way, there is not type of orchestration algorithm.
+    # This implementation is already created in selector.class,called: First_ShortestPath
     selectorPath = MinPath_RoundRobin()
 
     """
@@ -166,18 +221,19 @@ def main(simulated_time):
     """
     RUNNING - last step
     """
-    s.run(stop_time,show_progress_monitor=False)
+    s.run(stop_time, show_progress_monitor=False)
     s.print_debug_assignaments()
 
     # s.draw_allocated_topology() # for debugging
 
-    #s.draw_allocated_topology() # This implementation does not work when there is some node not linked
+    # s.draw_allocated_topology() # This implementation does not work when there is some node not linked
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import logging.config
     import os
 
-    logging.config.fileConfig(os.getcwd()+'/logging.ini')
+    logging.config.fileConfig(os.getcwd() + "/logging.ini")
 
     start_time = time.time()
     main(simulated_time=1000)
@@ -201,4 +257,4 @@ if __name__ == '__main__':
     # print(m.get_df_service_utilization("ServiceA",1000))
 
     # print("\n\t- Stats of each DEVICE -")
-    #TODO
+    # TODO
