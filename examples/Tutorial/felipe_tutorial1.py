9814a727c91453cb24b4b3f01acc08b096315a81
Felipe Ferreira Dumer <felipe.dumer@wexinc.com>
2025-12-07 16:59:50 -0300

    Fixing more BUGS, adds latency_analysis

--- examples/Tutorial/felipe_tutorial1.py
+++ examples/Tutorial/felipe_tutorial1.py
@@ -301,7 +301,7 @@ def createRandomTopology(
                 topology_object["entity"].append(
                     {
                         "id": sensor_id,
-                        "model": sensorNodeName,
+                        "model": f"Sensor-{sensor_id}",
                         "label": f"Sensor-{sensor_id}",
                         "x": router_x + random.uniform(-2, 2),
                         "y": device_y,
@@ -329,14 +329,14 @@ def createRandomTopology(
 
     return topology_object
 
-def createApplication():
+def createSimpleApplication(name: str) -> Application:
     """
     Creates the application definition with modules and messages.
 
     Returns:
         Application: The application object containing modules, messages, and services.
     """
-    applicationObject = Application(name="FelipeCase")
+    applicationObject = Application(name=name)
 
     sensor1Name = "SensorCollectingRawData"
     service1Name = "ServiceProcessingTheRequest"
@@ -364,6 +364,72 @@ def createApplication():
     applicationObject.add_service_module(service1Name, messageFromSensorToService, messageFromServiceToConsumer, fractional_selectivity, threshold=1.0)
 
     return applicationObject
+
+def createRealApplication_off(name: str) -> Application:
+    """
+    Creates a more complex application definition with modules and messages.
+
+    Returns:
+        Application: The application object containing modules, messages, and services.
+    """
+    applicationObject = Application(name=name)
+
+    sensorName = "Sensor"
+    serviceAName = "ServiceA"
+    serviceBName = "ServiceB"
+    sinkName = "Actuator"
+
+    # Creating modules
+    applicationObject.set_modules(
+        [
+            {sensorName: {"Type": Application.TYPE_SOURCE}},
+            {serviceAName: {"RAM": 10, "Type": Application.TYPE_MODULE}},
+            {serviceBName: {"RAM": 10, "Type": Application.TYPE_MODULE}},
+            {sinkName: {"Type": Application.TYPE_SINK}},
+        ]
+    )
+
+    # Creating messages
+    messageA = Message("M.A", sensorName, serviceAName, instructions=20 * 10**6, bytes=1000)
+    messageB = Message("M.B", serviceAName, serviceBName, instructions=30 * 10**6, bytes=500)
+    messageC = Message("M.C", serviceBName, sinkName, instructions=25 * 10**6, bytes=800)
+
+    # Source messages are controlled by population algorithm
+    applicationObject.add_source_messages(messageA)
+
+    # Modules redirect the messages
+    applicationObject.add_service_module(serviceAName, messageA, messageB, fractional_selectivity, threshold=1.0)
+    applicationObject.add_service_module(serviceBName, messageB, messageC, fractional_selectivity, threshold=1.0)
+
+    return applicationObject
+
+def create_application(name: str) -> Application:
+    # APLICATION
+    a = Application(name)
+
+    # (Sensor) --> (Service) --> (Actuator)
+    a.set_modules([{f"{name}-Sensor":{"Type":Application.TYPE_SOURCE}},
+                    {f"{name}-Service": {"RAM": 10, "Type": Application.TYPE_MODULE}},
+                    {f"{name}-Actuator": {"Type": Application.TYPE_SINK}}
+                    ])
+    """
+    Messages among MODULES (AppEdge in iFogSim)
+    """
+    m_a = Message("Sensor calling Service", f"{name}-Sensor", f"{name}-Service", instructions=20*10**6, bytes=1000)
+    m_b = Message("Service calling Actuator", f"{name}-Service", f"{name}-Actuator", instructions=30*10**6, bytes=500)
+
+    """
+    Defining which messages will be dynamically generated # the generation is controlled by Population algorithm
+    """
+    a.add_source_messages(m_a)
+
+    """
+    MODULES/SERVICES: Definition of Generators and Consumers (AppEdges and TupleMappings in iFogSim)
+    """
+    # MODULE SERVICES
+    a.add_service_module(f"{name}-Service", m_a, m_b, fractional_selectivity, threshold=1.0)
+
+    return a
 
 if __name__ == "__main__":
     import logging.config
@@ -423,35 +489,74 @@ if __name__ == "__main__":
     )
 
     # The application is being created using the function createApplication()
-    application = createApplication()
+    application1 = create_application("Application-1")
+    application2 = create_application("Application-2")
 
     # Initial placement if "mytag": "cloud"
-    placementAlgorithm = CloudPlacement("tagEqualsToCloud")
-    placementAlgorithm.scaleService({"ServiceProcessingTheRequest": 1})
+    placementAlgorithm1 = CloudPlacement("tagEqualsToCloud")
+    placementAlgorithm1.scaleService({f"Application-1-Service": 1, f"Application-2-Service": 1})
+
+    placementAlgorithm2 = CloudPlacement("tagEqualsToCloud")
+    placementAlgorithm2.scaleService({f"Application-2-Service": 1})
+
+    dDistribution = deterministic_distribution(name="Deterministic", time=100)
+
+    population_1 = Statical("Statical-1")
+    population_1.set_src_control({"model": "Sensor-23", "number":1,"message": application1.get_message("Sensor calling Service"), "distribution": dDistribution,"param": {"time_shift": 100}})#5.1}})
+    population_1.set_sink_control({"model": "Sensor-23","number":1,"module":application1.get_sink_modules()})
+
+    population_2 = Statical("Statical-2")
+    population_2.set_src_control({"model": "Sensor-34", "number":1,"message": application2.get_message("Sensor calling Service"), "distribution": dDistribution,"param": {"time_shift": 100}})#5.1}})
+    population_2.set_sink_control({"model": "Sensor-34","number":1,"module":application2.get_sink_modules()})
+
+
+    # # Population Algorithm
+    # populationAlgorithm1 = Statical("Statical")
+    # # For each type of sink modules we set a deployment on some type of devices
+    # # A control sink consists on:
+    # #  args:
+    # #     model (str): identifies the device or devices where the sink is linked
+    # #     number (int): quantity of sinks linked in each device
+    # #     module (str): identifies the module from the app who receives the messages
+    # populationAlgorithm1.set_sink_control(
+    #     {"model": actuatorNodeName, "number": 1, "module": application1.get_sink_modules()}
+    # )
+
+    # # In addition, a source includes a distribution function, This basically says send message every 50 simulation times
+    # distributionObject = deterministic_distribution(name="Deterministic", time=50)
+
+    # populationAlgorithm1.set_src_control(
+    #     {
+    #         "model": "Sensor-23",
+    #         "number": 1,
+    #         "message": application1.get_message("Hey This is Sensor Calling Service!"),
+    #         "distribution": distributionObject,
+    #     }
+    # )
 
     # Population Algorithm
-    populationAlgorithm = Statical("Statical")
+    # populationAlgorithm2 = Statical("Statical")
     # For each type of sink modules we set a deployment on some type of devices
     # A control sink consists on:
     #  args:
     #     model (str): identifies the device or devices where the sink is linked
     #     number (int): quantity of sinks linked in each device
     #     module (str): identifies the module from the app who receives the messages
-    populationAlgorithm.set_sink_control(
-        {"model": actuatorNodeName, "number": 1, "module": application.get_sink_modules()}
-    )
+    # populationAlgorithm2.set_sink_control(
+    #     {"model": actuatorNodeName, "number": 1, "module": application1.get_sink_modules()}
+    # )
 
     # In addition, a source includes a distribution function, This basically says send message every 50 simulation times
     distributionObject = deterministic_distribution(name="Deterministic", time=50)
 
-    populationAlgorithm.set_src_control(
-        {
-            "model": sensorNodeName,
-            "number": 1,
-            "message": application.get_message("Hey This is Sensor Calling Service!"),
-            "distribution": distributionObject,
-        }
-    )
+    # populationAlgorithm2.set_src_control(
+    #     {
+    #         "model": "Sensor-43",
+    #         "number": 1,
+    #         "message": application1.get_message("Hey This is Sensor Calling Service!"),
+    #         "distribution": distributionObject,
+    #     }
+    # )
 
     # Their "selector" is actually the shortest way, there is not type of orchestration algorithm.
     # This implementation is already created in selector.class,called: First_ShortestPath
@@ -460,7 +565,8 @@ if __name__ == "__main__":
     stop_time = 1000
 
     simulationObject = Sim(topology, default_results_path=results_path + "sim_trace")
-    simulationObject.deploy_app2(application, placementAlgorithm, populationAlgorithm, selectorPathAlgorithm)
+    simulationObject.deploy_app2(application1, placementAlgorithm1, population_1, selectorPathAlgorithm)
+    simulationObject.deploy_app2(application2, placementAlgorithm1, population_2, selectorPathAlgorithm)
 
     simulationObject.run(stop_time)
 
